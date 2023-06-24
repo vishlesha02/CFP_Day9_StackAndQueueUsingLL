94b8ff1a8b07b0afae4379243db88c963f78de9b
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-24 17:30:31 +0530

    Stack Added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
-﻿
-public class Program
-{
-    public static void Main(String[] args)
-    {
-        Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace StackAndQueueUsingLL
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            //Adds an Element 
+            Console.WriteLine("The pushed elements in stack are : ");
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Display();
+
+            //Removes an Element
+            Console.WriteLine("The elements after pop are : ");
+            stack.Pop();
+            stack.Display();
+
+            //Returns top most Element
+            stack.Peek();
+
+            //Checks if stack is Empty
+            stack.IsEmpty();
+            
+            //Checks the Size of Stack
+            stack.Size();
+            
+        }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueUsingLL
+{
+    internal class Stack<T>
+    {
+        Node<T> top;
+
+
+
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (top == null)
+            {
+                top = newNode;
+            }
+
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+
+        }
+
+
+        public T Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("The Stack is empty!!!");
+                return default(T);
+            }
+
+
+            T PopData = top.data;
+            top = top.next;
+            return PopData;
+        }
+
+
+        public void Peek()
+        {
+
+            Console.WriteLine("The Data on top is : " + top.data);
+            Console.WriteLine();
+
+        }
+
+
+        public void IsEmpty()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("The Stack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("The Stack is Not Empty");
+            }
+            Console.WriteLine();
+
+        }
+
+        public void Size()
+        {
+            int count = 0;
+
+            Node<T> temp = top;
+            while (temp != null) 
+            {
+                count++;
+                temp=temp.next;
+            }
+            Console.WriteLine("The size of Stack is : " + count);
+            Console.WriteLine();
+        }
+
+
+        public void Display() 
+        {
+            Node<T> temp = top;
+
+            while (temp != null) 
+            {
+                Console.WriteLine(temp.data);
+               temp=temp.next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
